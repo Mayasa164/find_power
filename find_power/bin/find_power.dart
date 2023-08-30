9b8bb4113bd56bf01e1cd7fdf19ebf1daffd0e6b
Mayasa164 <139007766+Mayasa164@users.noreply.github.com>
2023-08-30 09:38:17 +0400

    find_power

---
+++ find_power/bin/find_power.dart
@@ -0,0 +1,14 @@
+
+void main() {
+
+  int base = 1;
+  for (int expo = 0; expo < 2; expo++) {
+    base =base * 3;
+  }
+
+  print(base);
+
+}
+
+
+
---
+++ find_power/test/find_power_test.dart
@@ -0,0 +1,8 @@
+import 'package:find_power/find_power.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
